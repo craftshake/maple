98b42ea4cf122d2a1dd76b17a8fdd58effe81b54
Mario <mario@builtbysplash.com>
2013-11-27 01:06:57 +0100

    Remove useless stuff

--- resources/ts/Maps.ts
+++
@@ -1,8 +0,0 @@
-/// <reference path="vendor/jquery.d.ts" />
-/// <reference path="vendor/google.maps.d.ts" />
-/// <reference path="Map.ts" />
-/// <reference path="Geocoder.ts" />
-/// <reference path="MapFieldType.ts" />
-/// <reference path="LocationFieldType.ts" />
-
-google.maps.visualRefresh = true;
